aea227443e2ff27aab99e1379eb0bcaf73d24c73
keith wachira <keithwachira@gmail.com>
2022-12-01 19:41:39 +0300

    add gateway deployment

--- examples/main.tf
+++ examples/main.tf
@@ -7,28 +7,56 @@ terraform {
   }
 }
 
-provider "tyk" {}
+provider "tyk" {
 
+}
 data "tyk_org" "first" {
 
 }
-
 resource "tyk_team" "team" {
-  name = "terraform testing changed me"
+  name = "Testing tretararform team "
   oid  = data.tyk_org.first.uid
 }
 
 
 resource "tyk_env" "env" {
-  name     = "Terraform i changed name"
+  name     = "change this  Terraform i changed name"
   team_uid = tyk_team.team.uid
   org_id   = data.tyk_org.first.uid
 }
 
-resource "tyk_deployment" "deploy" {
-  name ="terraform deployment"
+resource "tyk_deployment" "home" {
+  name ="test home deployment deployment"
   team_uid = tyk_team.team.uid
   org_id   = data.tyk_org.first.uid
   zone_code=data.tyk_org.first.zone
   env_uid=tyk_env.env.uid
+  deploy= true
+  delete=true
+  purge=true
+}
+resource "tyk_deployment" "gateway" {
+  name ="terraform gateway deployment"
+  team_uid = tyk_team.team.uid
+  org_id   = data.tyk_org.first.uid
+  zone_code=data.tyk_org.first.zone
+  env_uid=tyk_env.env.uid
+  kind="Gateway"
+  deploy= true
+  delete=true
+  purge=true
+  linked_control_plane=tyk_deployment.home.uid
+}
+
+
+
+
+output "deployment" {
+  value = tyk_deployment.home
+}
+output "orgs" {
+  value = data.tyk_org.first
+}
+output "team" {
+  value = tyk_team.team
 }
--- tyk/resource_deployment.go
+++ tyk/resource_deployment.go
@@ -66,6 +66,10 @@ func resourceDeployment() *schema.Resource {
 				Optional: true,
 				Default:  false,
 			},
+			"linked_control_plane": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 		},
 	}
 }
@@ -77,11 +81,11 @@ func resourceDeploymentDelete(ctx context.Context, data *schema.ResourceData, m 
 	orgId := data.Get("org_id").(string)
 	teamUid := data.Get("team_uid").(string)
 	envUid := data.Get("env_uid").(string)
-	de := data.Get("delete").(bool)
+	//de := data.Get("delete").(bool)
 	purge := data.Get("purge").(bool)
 	_, _, err := client.DeploymentsApi.DestroyDeployment(ctx, orgId, teamUid, envUid, uid, &cloud.DeploymentsApiDestroyDeploymentOpts{
-		Delete: optional.NewBool(de),
-		Purge:  optional.NewBool(purge),
+		///Delete: optional.NewBool(false),
+		Purge: optional.NewBool(purge),
 	})
 	if err != nil {
 		return diag.FromErr(err)
@@ -96,22 +100,19 @@ func resourceDeploymentUpdate(ctx context.Context, data *schema.ResourceData, m 
 	orgId := data.Get("org_id").(string)
 	teamUid := data.Get("team_uid").(string)
 	envUid := data.Get("env_uid").(string)
-	kind := data.Get("kind").(string)
-	zone := data.Get("zone_code").(string)
+	///kind := data.Get("kind").(string)
+	///zone := data.Get("zone_code").(string)
 	if data.HasChanges("name") {
 		name := data.Get("name").(string)
 		deployment := cloud.Deployment{
-			BundleChannel:     "",
-			BundleVersion:     "",
-			Kind:              kind,
-			LID:               envUid,
-			LinkedDeployments: nil,
-			Name:              name,
-			TID:               teamUid,
-			ZoneCode:          zone,
+			Name: name,
 		}
-		_, _, err := client.DeploymentsApi.UpdateDeployment(ctx, deployment, orgId, teamUid, envUid, uid, nil)
+		payload, _, err := client.DeploymentsApi.UpdateDeployment(ctx, deployment, orgId, teamUid, envUid, uid, nil)
 		if err != nil {
+			log.Println("error is:", err)
+			log.Println("here it is", payload)
+			log.Println("here it is", payload.Error_)
+			log.Println("here it is", payload.Payload)
 			return diag.FromErr(err)
 		}
 		if err := data.Set("last_updated", time.Now().Format(time.RFC850)); err != nil {
@@ -128,7 +129,11 @@ func resourceDeploymentRead(ctx context.Context, data *schema.ResourceData, m in
 	orgId := data.Get("org_id").(string)
 	teamUid := data.Get("team_uid").(string)
 	envUid := data.Get("env_uid").(string)
-	deployment, _, err := client.DeploymentsApi.GetDeployment(ctx, orgId, teamUid, envUid, uid, nil)
+	deployment, resp, err := client.DeploymentsApi.GetDeployment(ctx, orgId, teamUid, envUid, uid, nil)
+	if resp.StatusCode == 404 {
+		data.SetId("")
+		return diags
+	}
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -136,6 +141,9 @@ func resourceDeploymentRead(ctx context.Context, data *schema.ResourceData, m in
 		return diag.FromErr(err)
 	}
 	if err := data.Set("zone_code", deployment.Payload.ZoneCode); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("uid", deployment.Payload.UID); err != nil {
 		return diag.FromErr(err)
 	}
 	return diags
@@ -151,13 +159,27 @@ func resourceDeploymentCreate(ctx context.Context, data *schema.ResourceData, m 
 	kind := data.Get("kind").(string)
 	zone := data.Get("zone_code").(string)
 	deploy := data.Get("deploy").(bool)
+	linkedControlPlane := data.Get("linked_control_plane").(string)
 	deployment := cloud.Deployment{
 		Kind:              kind,
 		LID:               envUid,
-		LinkedDeployments: nil,
-		Name:              name,
-		TID:               teamUid,
-		ZoneCode:          zone,
+		LinkedDeployments: map[string]string{},
+		LastUpdate:        time.Now().UTC(),
+		Created:           time.Now().UTC(),
+		Driver:            "K8s_sp",
+		DriverMetaData: &cloud.Status{
+			CurrentState: "starting",
+			Timestamp:    time.Now().UTC(),
+		},
+		ExtraContext: &cloud.MetaDataStore{
+			Data: map[string]map[string]interface{}{},
+		},
+		Name:     name,
+		TID:      teamUid,
+		ZoneCode: zone,
+	}
+	if linkedControlPlane != "" && deployment.Kind == "Gateway" {
+		deployment.LinkedDeployments["LinkedMDCBID"] = linkedControlPlane
 	}
 	createDeployment, _, err := client.DeploymentsApi.CreateDeployment(ctx, deployment, orgId, teamUid, envUid)
 	if err != nil {
